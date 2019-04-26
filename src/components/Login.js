c7d9985ebce0aace560ecc1b1b167b20cd603d7a
Prabhjot Singh <prabhjot.narula@gmail.com>
2019-04-26 11:54:05 -0400

    Committing Login Page UI Changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -19,49 +19,53 @@ export default class Login extends Component {
 
   handleChange(e) {
     this.setState({
-      [event.target.id]: event.target.value
+      [e.target.id]: e.target.value
     });
   }
 
   handleSubmit(e){
-    event.preventDefault();
+    e.preventDefault();
   }
 
   render() {
-    return (
-      <div className="Login">
-        <Form onSubmit={this.handleSubmit} >
-          <Form.Group className="Username" controlId="username">
-            <Form.Label >Username</Form.Label>
-            <Form.Control
-              autoFocus
-              type="text"
-              value={this.state.username}
-              onChange={this.handleChange}
-              placeholder="Username"
-            />
-          </Form.Group>
-          <Form.Group className="Password" controlId="password">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              value={this.state.password}
-              onChange={this.handleChange}
-              type="password"
-              placeholder="Password"
-            />
-          </Form.Group>
-          <Button
-            block
-            disabled={!this.validateForm()}
-            type="submit"
-          >
-            Login
-          </Button>
-        </Form>
-        <div className="OR">OR</div>
-        <div className="TxtLogin">Login with your Twitch Account</div>
-        <img src="{twitch}"></img>
-      </div>
-    );
+    return (<div>
+        <div><img src="https://images.pexels.com/photos/1308624/pexels-photo-1308624.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"/></div>
+        <div className="Login">
+            <h3 className="Heading">Sign In</h3>
+            <Form onSubmit={this.handleSubmit} >
+            <Form.Group className="Username" controlId="username">
+                <Form.Label >Username</Form.Label>
+                <Form.Control
+                autoFocus
+                type="text"
+                value={this.state.username}
+                onChange={this.handleChange.bind(this)}
+                placeholder="Username"
+                />
+            </Form.Group>
+            <Form.Group className="Password" controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                value={this.state.password}
+                onChange={this.handleChange.bind(this)}
+                type="password"
+                placeholder="Password"
+                />
+            </Form.Group>
+            <Button
+                block
+                disabled={!this.validateForm()}
+                type="submit"
+            >
+                Login
+            </Button>
+            </Form>
+            <div className="TwitchSection">
+                <div className="OR">OR</div>
+                <div className="TxtLogin">Login with your Twitch Account</div>
+                <Button className="BtnTwitch" type="submit"><img height='60px' src="https://drops-register.ubi.com/img/logo_twitch_general.png"></img></Button>
+            </div>
+        </div>
+    </div>);
   }
 }
