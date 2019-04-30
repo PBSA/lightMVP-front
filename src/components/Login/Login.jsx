743dcd168be83e8581f7d9d699680111775d904c
Michael Seaward <mamokinmedia@gmail.com>
2019-04-30 08:46:53 -0300

    fix(login form): binded functions
    without binding, the functions such as handleChange() will error. Added babel package to handle
    "proposal class properties" that allow us to use arrow functions to bind these functions rather than
    cumbersome code such as: `this.handChange = this.handleChange.bind(this)` within the class
    constructor.

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,65 +1,68 @@
-import React, { Component } from "react";
-import { Button, Form } from "react-bootstrap";
-import twitch from "../../img/twitch.png";
+import React, {Component} from 'react';
+import {Button, Form} from 'react-bootstrap';
+import twitch from '../../img/twitch.png';
 
 export default class Login extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      username: "",
-      password: ""
+      username: '',
+      password: ''
     };
+
   }
 
-  validateForm() {
+  validateForm = () => {
     return this.state.username.length > 0 && this.state.password.length > 0;
   }
 
-  handleChange(e) {
+  handleChange = (e) => {
     this.setState({
-      [event.target.id]: event.target.value
+      [e.target.id]: e.target.value
     });
   }
 
-  handleSubmit(e){
-    event.preventDefault();
+  handleSubmit = (e) => {
+    e.preventDefault();
   }
 
   render() {
     return (
-      <div className="Login">
-        <Form onSubmit={this.handleSubmit} >
-          <Form.Group className="Username" controlId="username">
+      <div className='Login'>
+        <Form onSubmit={ this.handleSubmit } >
+          <Form.Group className='Username' controlId='username'>
             <Form.Label >Username</Form.Label>
             <Form.Control
               autoFocus
-              type="text"
-              value={this.state.username}
-              onChange={this.handleChange}
-              placeholder="Username"
+              type='text'
+              value={ this.state.username }
+              onChange={ this.handleChange }
+              placeholder='Username'
+              autoComplete='username'
             />
           </Form.Group>
-          <Form.Group className="Password" controlId="password">
+          <Form.Group className='Password' controlId='password'>
             <Form.Label>Password</Form.Label>
             <Form.Control
-              value={this.state.password}
-              onChange={this.handleChange}
-              type="password"
-              placeholder="Password"
+              value={ this.state.password }
+              onChange={ this.handleChange }
+              type='password'
+              placeholder='Password'
+              autoComplete='current-password'
             />
           </Form.Group>
           <Button
             block
-            disabled={!this.validateForm()}
-            type="submit"
+            disabled={ !this.validateForm() }
+            type='submit'
           >
             Login
           </Button>
         </Form>
-        <div className="OR">OR</div>
-        <div className="TxtLogin">Login with your Twitch Account</div>
-        <img src={twitch}></img>
+        <div className='OR'>OR</div>
+        <div className='TxtLogin'>Login with your Twitch Account</div>
+        <img src={ twitch } alt='twitch'></img>
       </div>
     );
   }
